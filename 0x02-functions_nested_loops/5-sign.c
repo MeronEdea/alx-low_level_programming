0ddbcea24068d1ba7229026afe2f42d3dd93dffe
Meron Edea <edeameron@gmail.com>
2023-09-13 04:56:22 -0900

    functions in c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,19 +11,22 @@
  */
 int print_sign(int n)
 {
-	if (n > 0)
-	{
-		putchar('+');
-		return (1);
-	}
-	else if (n < 0)
-	{
-		putchar('-');
-		return (-1);
-	}
-	else
-	{
-		putchar('0');
-		return (0);
-	}
+	 if (n > 0)
+  {
+    putchar('+');
+    putchar('\n');
+    return (1);
+  }
+  else if (n < 0)
+  {
+    putchar('-');
+    putchar('\n');
+    return (-1);
+  }
+  else
+  {
+    putchar('0');
+    putchar('\n');
+    return (0);
+  }
 }
