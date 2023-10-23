0ebfacace0eb7ce1cfdb020ed75090282fcd638c
Meron Edea <edeameron@gmail.com>
2023-10-23 06:56:08 -0900

    adding files

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-gcc -c *.c
+# Get the list of .c files in the current directory
+c_files=$(find . -maxdepth 1 -type f -name "*.c")
+
+# Compile each .c file into an object file
+for file in $c_files; do
+    gcc -c "$file"
+done
+
+# Create the static library liball.a
 ar rcs liball.a *.o
